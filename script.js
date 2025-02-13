35473d164810cef8435f7c067b6ce39b2e7a5c95
Ajay <ajay412004@gmail.com>
2025-02-13 16:58:08 +0530

    Fixed know issues

--- script.js
+++ script.js
@@ -16,62 +16,75 @@ submit.addEventListener("click", (e) => {
     if (userName.value == "") {
         nameError.innerHTML = "Please Enter Your Name";
         nameError.classList.add("showerror");
+        userName.classList.add("error-border");
         errorFlag = true;
     }
     if (email.value == "") {
         emailError.innerHTML = "Please Enter Your Email";
         emailError.classList.add("showerror");
+        email.classList.add("error-border");
         errorFlag = true;
     }
     if (email.value != "") {
         if (email.value.indexOf("@") < 1 || email.value.lastIndexOf(".") < email.value.indexOf("@") + 2 || email.value.lastIndexOf(".") + 2 >= email.value.length) {
             emailError.innerHTML = "Please Enter A Valid Email Address"
             emailError.classList.add("showerror")
+            email.classList.add("error-border");
             errorFlag = true;
         }
     }
     if (password.value == "") {
         passError.innerHTML = "Please Enter Your Password";
         passError.classList.add("showerror");
+        password.classList.add("error-border");
         errorFlag = true;
     }
     if (password.value != "") {
         if (password.value.length < 8) {
             passError.innerHTML = "Password Must Be At Least 8 Characters"
             passError.classList.add("showerror")
+            password.classList.add("error-border");
             errorFlag = true;
         }
-        if (password.value.search(/[0-9]/) == -1) {
+        else if (password.value.search(/[0-9]/) == -1) {
             passError.innerHTML = "Password Must Contain At Least One Number"
             passError.classList.add("showerror")
+            password.classList.add("error-border");
             errorFlag = true;
         }
-        if (password.value.search(/[a-z]/) == -1) {
+        else if (password.value.search(/[a-z]/) == -1) {
             passError.innerHTML = "Password Must Contain At Least One Lowercase Letter"
             passError.classList.add("showerror")
+            password.classList.add("error-border");
             errorFlag = true;
         }
-        if (password.value.search(/[A-Z]/) == -1) {
+        else if (password.value.search(/[A-Z]/) == -1) {
             passError.innerHTML = "Password Must Contain At Least One Uppercase Letter"
             passError.classList.add("showerror")
+            password.classList.add("error-border");
             errorFlag = true;
         }
-        if (password.value.search(/[!@#$%^&*]/) == -1) {
+        else if (password.value.search(/[!@#$%^&*]/) == -1) {
             passError.innerHTML = "Password Must Contain At Least One Special Character"
             passError.classList.add("showerror")
+            password.classList.add("error-border");
             errorFlag = true;
         }
     }
     if (password2.value == "") {
         pass2Error.innerHTML = "Please Confirm Your Password"
         pass2Error.classList.add("showerror")
+        password2.classList.add("error-border");
         errorFlag = true;
     }
+    if (password2.value != "") {
 
-    if (password.value != password2.value) {
-        pass2Error.innerHTML = "Passwords Do Not Match"
-        pass2Error.classList.add("showerror")
-        errorFlag = true;
+        if (password.value != password2.value) {
+            pass2Error.innerHTML = "Passwords Do Not Match"
+            pass2Error.classList.add("showerror")
+            password2.classList.add("error-border");
+            errorFlag = true;
+        }
     }
     if (errorFlag) {
         const shakeElements = [userName, email, password, password2];
@@ -92,16 +105,21 @@ submit.addEventListener("click", (e) => {
 userName.addEventListener("input", () => {
     nameError.innerHTML = ""
     nameError.classList.remove("showerror")
+    userName.classList.remove("error-border");
 })
 email.addEventListener("input", () => {
     emailError.innerHTML = ""
     emailError.classList.remove("showerror")
+    email.classList.remove("error-border");
 })
 password.addEventListener("input", () => {
     passError.innerHTML = ""
     passError.classList.remove("showerror")
+    password.classList.remove("error-border");
 })
 password2.addEventListener("input", () => {
     pass2Error.innerHTML = ""
     pass2Error.classList.remove("showerror")
+    password2.classList.remove("error-border");
+
 })
